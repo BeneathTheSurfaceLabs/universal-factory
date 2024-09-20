991fb27f8272812603e248e802797b0c46e00d93
Nick Poulos <nickpoulos.ny@gmail.com>
2024-09-19 23:36:49 -0400

    Create universal-factory.php

---
+++ config/universal-factory.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Namespace for Universal Factories
+    |--------------------------------------------------------------------------
+    |
+    | This value defines the default namespace for the universal factories. You can
+    | change it to fit your application's needs.
+    |
+    */
+    'default_namespace' => 'App\\Factories\\',
+];
