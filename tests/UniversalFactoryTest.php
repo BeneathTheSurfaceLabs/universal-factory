e80edcc81847a6426afc788e45ada8c00b8c4e26
Nick Poulos <nickpoulos.ny@gmail.com>
2024-09-22 00:17:50 -0400

    Update UniversalFactoryTest.php
    Add additional tests for nested factories

--- tests/UniversalFactoryTest.php
+++ tests/UniversalFactoryTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use BeneathTheSurfaceLabs\UniversalFactory\Tests\Examples\ProfileData;
 use BeneathTheSurfaceLabs\UniversalFactory\Tests\Examples\UserInfo;
 use BeneathTheSurfaceLabs\UniversalFactory\Tests\Examples\UserInfoFactory;
 use Illuminate\Support\Str;
@@ -12,11 +13,15 @@ it('Can Create A New Factory Instance', function () {
 it('Can Make A New Class via Factory With Empty State', function () {
     $factory = UserInfo::factory();
     $result = $factory->make();
-
     expect($result->age)->toBeBetween(21, 40);
     expect($result->name)->toBeString();
     expect(Str::of($result->email)->contains('@'))->toBeTrue();
     expect($result->birthday)->toBeInstanceOf(\DateTime::class);
+    expect($result->profileData)->toBeInstanceOf(ProfileData::class);
+    expect($result->profileData->facebookProfileUrl)->toContain('https://facebook.com/');
+    expect($result->profileData->twitterProfileUrl)->toContain('https://x.com/');
+    expect($result->profileData->gitHubProfileUrl)->toContain('https://github.com/');
+
 });
 
 it('Can Make A Many New Classes via Factory With Empty State', function () {
@@ -69,12 +74,12 @@ it('It Can Resolve The Base Classes From The Factories', function (string $facto
     $className = app($factoryClassName)->className();
     expect($className)->toEqual($expectedClassName);
 })->with([
-    "UserInfoFactory" => [UserInfoFactory::class, UserInfo::class],
+    'UserInfoFactory' => [UserInfoFactory::class, UserInfo::class],
 ]);
 
 it('It Can Resolve The Factory Class From The Base Classes', function (string $className, string $expectedFactoryClassName) {
     $factory = $className::factory()::resolveFactoryName($className);
     expect($factory)->toEqual($expectedFactoryClassName);
 })->with([
-    "UserInfo" => [UserInfo::class, UserInfoFactory::class, ],
+    'UserInfo' => [UserInfo::class, UserInfoFactory::class],
 ]);
