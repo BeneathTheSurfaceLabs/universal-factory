59d53916a8eb6cea7c25b60145ff80f3f460c702
Nick Poulos <nickpoulos.ny@gmail.com>
2024-09-22 00:16:51 -0400

    Create additional support/example classes for testing
    These will help show afterMaking callbacks

---
+++ tests/Examples/ProfileData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BeneathTheSurfaceLabs\UniversalFactory\Tests\Examples;
+
+use BeneathTheSurfaceLabs\UniversalFactory\Traits\HasUniversalFactory;
+
+class ProfileData
+{
+    use HasUniversalFactory;
+
+    public function __construct(
+        public ?string $facebookProfileUrl,
+        public ?string $facebookAvatarUrl,
+        public ?string $twitterProfileUrl,
+        public ?string $twitterAvatarUrl,
+        public ?string $gitHubProfileUrl,
+        public ?string $githubAvatarUrl,
+        public ?string $personalUrl,
+    ) {}
+
+    public static function newFactory(): ProfileDataFactory
+    {
+        return ProfileDataFactory::new();
+    }
+}
---
+++ tests/Examples/ProfileDataFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace BeneathTheSurfaceLabs\UniversalFactory\Tests\Examples;
+
+use BeneathTheSurfaceLabs\UniversalFactory\UniversalFactory;
+use Illuminate\Support\Str;
+
+class ProfileDataFactory extends UniversalFactory
+{
+    protected $class = ProfileData::class;
+
+    /**
+     * Define the class's default attributes.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'facebookProfileUrl' => fake()->url(),
+            'facebookAvatarUrl' => fake()->imageUrl(),
+            'twitterProfileUrl' => fake()->url(),
+            'twitterAvatarUrl' => fake()->imageUrl(),
+            'gitHubProfileUrl' => fake()->url(),
+            'githubAvatarUrl' => fake()->imageUrl(),
+            'personalUrl' => fake()->url(),
+        ];
+    }
+
+    public function withProfileFor(UserInfo $userInfo): self
+    {
+        $usernameGenerator = function (UserInfo $userInfo) {
+            $method = fake()->boolean() ? 'slug' : 'studly';
+
+            return Str::$method(
+                $userInfo->name,
+                fake()->randomElement(['-', '_', '.']).
+                (fake()->boolean() ? $userInfo->birthday->format(fake()->randomElement(['Y', 'y', 'my'])) : ''),
+
+            );
+        };
+
+        $urls = [
+            'facebook' => 'https://facebook.com/'.$usernameGenerator($userInfo),
+            'twitter' => 'https://x.com/'.$usernameGenerator($userInfo),
+            'github' => 'https://github.com/'.$usernameGenerator($userInfo),
+            'personal' => 'https://'.Str::slug($userInfo->name).'.com/',
+        ];
+
+        return $this->state(function (array $attributes) use ($urls) {
+            $attributes['facebookProfileUrl'] = $urls['facebook'];
+            $attributes['twitterProfileUrl'] = $urls['twitter'];
+            $attributes['gitHubProfileUrl'] = $urls['github'];
+            $attributes['personalUrl'] = $urls['personal'];
+
+            return $attributes;
+        });
+    }
+}
