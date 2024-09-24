c303a21dddf5eaa092d22a3296a5c3f9c075a13a
Nick Poulos <nickpoulos.ny@gmail.com>
2024-09-24 01:05:24 -0400

    Create Custom Drivers For Archetype

---
+++ src/Support/ArchetypeInputDriver.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BeneathTheSurfaceLabs\UniversalFactory\Support;
+
+use Archetype\Support\PHPFileStorage;
+use Archetype\Drivers\InputInterface;
+
+class ArchetypeInputDriver implements InputInterface
+{
+    public ?string $filename = '';
+
+    public ?string $extension = '';
+    public ?string $relativeDir = '';
+    public ?string $absoluteDir = '';
+
+    /**
+     * @throws \Exception
+     */
+    public function load(string $path = null)
+    {
+        try {
+            $reflection = new \ReflectionClass($path);
+            $classFilePath = $reflection->getFileName();
+            $this->absoluteDir = dirname($classFilePath);
+            $this->filename = $reflection->getShortName();
+            $this->extension = pathinfo($classFilePath, PATHINFO_EXTENSION);
+            return (new PHPFileStorage())->get($classFilePath);
+        } catch (\ReflectionException $e) {
+            throw new \Exception("Archetype Input Driver: Class {$path} could not be found.");
+        }
+    }
+
+}
