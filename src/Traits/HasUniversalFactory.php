826b6608216b45bdb02ed87f91a8a0b5d9717123
Nick Poulos <nickpoulos.ny@gmail.com>
2024-09-25 22:32:11 -0400

    Update the HasFactory trait to check for custom method names

--- src/Traits/HasUniversalFactory.php
+++ src/Traits/HasUniversalFactory.php
@@ -10,13 +10,29 @@ use BeneathTheSurfaceLabs\UniversalFactory\UniversalFactory;
 trait HasUniversalFactory
 {
     /**
+     * Dynamically provide the factory method for the model based on config.
+     */
+    public static function __callStatic($method, $arguments)
+    {
+        $factoryMethodName = config('universal-factory.method_name', 'factory');
+
+        // Check if the called method is the factory method
+        if ($method === $factoryMethodName) {
+            return self::getUniversalFactory(...$arguments);
+        }
+
+        // If method doesn't match, continue as normal
+        throw new \BadMethodCallException("Method {$method} does not exist.");
+    }
+
+    /**
      * Get a new factory instance for the model.
      *
      * @param  (callable(array<string, mixed>, static|null): array<string, mixed>)|array<string, mixed>|int|null  $count
      * @param  (callable(array<string, mixed>, static|null): array<string, mixed>)|array<string, mixed>  $state
      * @return TUniversalFactory
      */
-    public static function factory($count = null, $state = [])
+    protected static function getUniversalFactory($count = null, $state = [])
     {
         // Get factory for this class using the base factory logic
         $factory = static::newFactory() ?? UniversalFactory::factoryForClass(get_called_class());
